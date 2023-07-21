5f96945e1e1a51cfe220b66adf3dac7b7a766703
Amanda Lins <amandalinsdc@gmail.com>
2023-07-21 08:55:07 -0300

    Criar teste unitário de lista por genero feminino

---
+++ mod23/test/Testes/StreamsTest.java
@@ -0,0 +1,25 @@
+package mod23.Testes;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsTest {
+
+    mainStreams testar;
+
+    @Test
+    public void getTodosDoGeneroFemininoTest() {
+        List<String> pessoas = Arrays.asList("Amanda (f)", "José (m)", "Pedro (M)", "Paula (F)", "Neide (f)");
+        List<String> generoFeminino = pessoas.stream().filter(p -> p.contains("f") || p.contains("F")).collect(Collectors.toList());
+
+        Assert.assertEquals(3, generoFeminino.size());
+        for(String f : generoFeminino) {
+            Assert.assertTrue(f.toLowerCase().contains("f"));
+        }
+    }
+}
