bc962a6c68164deb16c53be123b8bad474b70f5b
Amanda Lins <amandalinsdc@gmail.com>
2023-09-25 21:44:35 -0300

    Primeiro projeto spring boot

---
+++ mod40/projetoSpring/src/main/java/com/ebac/amanda/projetoSpring/ProjetoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.ebac.amanda.projetoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Primeiro projeto Spring Boot");
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
